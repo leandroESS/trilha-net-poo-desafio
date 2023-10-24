0b31a3a5d6a1f53b7b2faaaf12600329afb302a2
Leandro Andrade <leandroxd.ec@gmail.com>
2023-10-24 10:15:54 -0300

    Desafio concluído com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "12345", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
